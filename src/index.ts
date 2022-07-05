438b2a5aca3bc737a662eb3be0ac4828482e8425
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-07-05 18:21:46 +0530

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { copyFileSync, existsSync, mkdirSync } from "fs";
 import { copyFile, mkdir, readdir, readFile, unlink, writeFile } from "fs/promises";
 import { join } from "path";
-import downloadVariables from "./downloadVariables";
 declare var require;
 
 /**
@@ -23,39 +22,6 @@ function update(target, key: string, value) {
     target[last] = value;
 }
 
-async function deleteFiles(folder) {
-    const items = await readdir(folder, { withFileTypes: true });
-    const tasks = [];
-    for (const iterator of items) {
-        const path = join(folder, iterator.name);
-        if (iterator.isDirectory()) {
-            tasks.push(deleteFiles(path));
-            continue;
-        }
-        tasks.push(unlink(path));
-    }
-    await Promise.all(tasks);
-}
-
-async function copyFiles(source, destination) {
-    if (!existsSync(destination)) {
-        mkdirSync(destination);
-    }
-    const items = await readdir(source, { withFileTypes: true });
-    const tasks = [];
-    for (const iterator of items) {
-        const path = join(source, iterator.name);
-        const dest = join(destination, iterator.name);
-        if (iterator.isDirectory()) {
-            tasks.push(copyFiles(path, dest));
-            continue;
-        }
-        console.log(`Copying ${path} to ${dest}`);
-        tasks.push(copyFile(path, dest));
-    }
-    await Promise.all(tasks);
-}
-
 function getArg(name) {
     const index = process.argv.indexOf(name);
     return process.argv[index + 1];
@@ -64,15 +30,13 @@ function getArg(name) {
 async function setup() {
 
     const source = getArg("--src");
-    const destination = getArg("--dest");
-    const variablesRpoHost = getArg("--vars-host") || process.env.CI_SERVER_HOST;
-    const variablesRepo = getArg("--vars-repo");
-    const varsToken = process.env.CI_JOB_TOKEN;
 
-    // copy files to destination...
-    // await deleteFiles(destination);
+    const name = getArg("--vars");
 
-    const variables = await downloadVariables(variablesRpoHost, variablesRepo, varsToken, getArg("--vars"));
+    const variablesDir = join(process.cwd(), "dv");
+    const js = join(variablesDir, name);
+
+    const variables = require(js);
 
     const appSettingsFile = join(source, "appsettings.json");
 
@@ -87,8 +51,6 @@ async function setup() {
     }
 
     await writeFile(appSettingsFile, JSON.stringify(appSettings, undefined, 2), { encoding: "utf-8" });
-
-    await copyFiles(source, destination);
 
 }
 
