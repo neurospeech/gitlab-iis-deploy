3224c2468ce05e630a096ec4adf6ba6aebd8d2d0
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-07-05 11:09:51 +0530

    download removed

--- src/downloadVariables.ts
+++ src/downloadVariables.ts
@@ -6,38 +6,38 @@ import { join } from "path";
 
 export default async function downloadVariables(host: string, id: string, jobToken: string, name: string) {
 
-    const variablesDir = join(process.cwd(), "dv");
-    if (!existsSync(variablesDir)) {
-        await mkdir(variablesDir);
-    }
+    // const variablesDir = join(process.cwd(), "dv");
+    // if (!existsSync(variablesDir)) {
+    //     await mkdir(variablesDir);
+    // }
 
-    console.log(`Connecting to git at ${host} with jobToken`);
+    // console.log(`Connecting to git at ${host} with jobToken`);
 
-    const api = new Gitlab({
-        host,
-        jobToken
-    });
+    // const api = new Gitlab({
+    //     host,
+    //     jobToken
+    // });
 
-    console.log(`Querying master branch files for repo ${id}`);
+    // console.log(`Querying master branch files for repo ${id}`);
 
-    const tree = await api.Repositories.tree(id);
-    const files = [];
-    const tasks = [];
-    for (const iterator of tree) {
-        const path = join(variablesDir, iterator.path);
-        files.push(path);
-        console.log(`Downloading file ${iterator.path} from ${id} to ${path}`);
-        tasks.push(writeFile(path, await api.RepositoryFiles.showRaw(id, iterator.path)));
-    }
-    await Promise.all(tasks);
+    // const tree = await api.Repositories.tree(id);
+    // const files = [];
+    // const tasks = [];
+    // for (const iterator of tree) {
+    //     const path = join(variablesDir, iterator.path);
+    //     files.push(path);
+    //     console.log(`Downloading file ${iterator.path} from ${id} to ${path}`);
+    //     tasks.push(writeFile(path, await api.RepositoryFiles.showRaw(id, iterator.path)));
+    // }
+    // await Promise.all(tasks);
 
-    const js = join(variablesDir, name);
+    const js = join("dv", name);
 
     const variables = require(js);
 
-    for (const iterator of files) {
-        await unlink(iterator);
-    }
+    // for (const iterator of files) {
+    //     await unlink(iterator);
+    // }
 
     return variables;
 }
