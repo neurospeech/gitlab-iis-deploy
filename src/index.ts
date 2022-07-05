6d19902113ec1f7aa37b9ba5f34c7d1a5891cfb2
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-07-05 10:03:44 +0530

    Added file copy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { copyFileSync, existsSync } from "fs";
-import { mkdir, readdir, readFile, unlink, writeFile } from "fs/promises";
+import { copyFile, mkdir, readdir, readFile, unlink, writeFile } from "fs/promises";
 import { join } from "path";
 import downloadVariables from "./downloadVariables";
 declare var require;
@@ -27,12 +27,30 @@ async function deleteFiles(folder) {
     await Promise.all(tasks);
 }
 
+async function copyFiles(source, destination) {
+    const items = await readdir(source, { withFileTypes: true });
+    const tasks = [];
+    for (const iterator of items) {
+        const path = join(source, iterator.name);
+        const dest = join(destination, iterator.name);
+        if (iterator.isDirectory()) {
+            tasks.push(copyFiles(path, dest));
+            continue;
+        }
+        tasks.push(copyFile(path, dest));
+    }
+    await Promise.all(tasks);
+}
+
 function getArg(name) {
     const index = process.argv.indexOf(name);
     return process.argv[index + 1];
 }
 
-async function setup(source, destination) {
+async function setup() {
+
+    const source = getArg("--src");
+    const destination = getArg("--dest");
 
     // copy files to destination...
     await deleteFiles(destination);
